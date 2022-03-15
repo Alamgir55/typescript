d3dea3657efd341fb2b43c22175585a04944fd58
alamgir hossain <alamgir555hossain@gmail.com>
2022-03-15 05:02:11 -0400

    basic-typescript

---
+++ features/arrays.ts
@@ -0,0 +1,18 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = []
+
+const myCar = carMakers[0];
+const myCar1 = carMakers.pop();
+
+carMakers.push()
+
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+const importantDate: (Date | string)[] = [new Date()];
+importantDate.push('2030-10-10');
+importantDate.push(new Date());
+
